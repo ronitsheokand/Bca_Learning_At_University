c3b97efec445af9760472ba2d613f0c4b84899ea
ronit7202 <128242677+ronitsheokand@users.noreply.github.com>
2023-04-27 16:33:30 +0530

    Update Assignment1.c

--- Assignment1.c
+++ Assignment1.c
@@ -1,7 +1,23 @@
 //Write a C program that allows the user to enter 'N' numbers and find the total positive numbers and total negative numbers using a for loop.
 
+#include<stdio.h>
 int main()
 {
-  //add your code here
-  return 0;
+	int i,p=0,n=0,j,s;
+	printf("Enter a number of inputs: ");
+	scanf("%d",&j);
+	for(i=0;i<j;i++)
+	{
+		scanf("%d",&s);
+		if(s>0)
+		{
+			p+=1;
+		}
+		else
+		{
+			n+=1;
+		}
+	}
+	printf("Number of positive number are: %d\n",p);
+	printf("Number of negative number are: %d",n);
 }
