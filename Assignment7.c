b9b6ae5f2e1f32562634eb0b9e564ef3cdb9d55b
ronit7202 <128242677+ronitsheokand@users.noreply.github.com>
2023-04-27 16:41:09 +0530

    Update Assignment7.c

--- Assignment7.c
+++ Assignment7.c
@@ -1,6 +1,23 @@
 //Write a code find the length of a string and its reverse equivalent without using readymade c functions
 
-int main()
-{
+#include <stdio.h>
+
+int main() {
+  char str[] = "sagar garg";
+  int l = 0,i;
+  while (str[l] != '\0')
+  {
+    l++;
+  }
+  printf("Length of string is %d\n",l);
+  
+  char reserved[l];
+  for (int i=0;i<l;i++) 
+  {
+    reserved[i] = str[l-1-i];
+  }
+  reserved[l] = '\0';
+  printf("Reversed string is %s",reserved);
+
   return 0;
 }
