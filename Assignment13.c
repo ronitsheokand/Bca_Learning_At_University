f72b1953a1fafbfca6bb0d95a9a8d24b2b5c6398
ronit7202 <128242677+ronitsheokand@users.noreply.github.com>
2023-04-27 16:48:34 +0530

    Update Assignment13.c

--- Assignment13.c
+++ Assignment13.c
@@ -6,7 +6,55 @@
 
 //after showing result the main menu should appear again.
 
-int main()
-{
-  return 0;
+
+#include <stdio.h>
+
+int main() {
+    int choice;
+    float num1, num2, result;
+
+    do {
+        printf("\nMenu:\n");
+        printf("1. Addition\n");
+        printf("2. Subtraction\n");
+        printf("3. Multiplication\n");
+        printf("4. Division\n");
+        printf("Enter your choice (1-4): ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter two numbers to add: ");
+                scanf("%f%f", &num1, &num2);
+                result = num1 + num2;
+                printf("Result: %.2f\n", result);
+                break;
+            case 2:
+                printf("Enter two numbers to subtract: ");
+                scanf("%f%f", &num1, &num2);
+                result = num1 - num2;
+                printf("Result: %.2f\n", result);
+                break;
+            case 3:
+                printf("Enter two numbers to multiply: ");
+                scanf("%f%f", &num1, &num2);
+                result = num1 * num2;
+                printf("Result: %.2f\n", result);
+                break;
+            case 4:
+                printf("Enter two numbers to divide: ");
+                scanf("%f%f", &num1, &num2);
+                if (num2 == 0) {
+                    printf("Error: division by zero\n");
+                } else {
+                    result = num1 / num2;
+                    printf("Result: %.2f\n", result);
+                }
+                break;
+            default:
+                printf("Invalid choice\n");
+        }
+    } while (1);
+    
+    return 0;
 }
