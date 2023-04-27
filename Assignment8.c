dcddbc12dde830429a002e7e1421b6afce87aa47
ronit7202 <128242677+ronitsheokand@users.noreply.github.com>
2023-04-27 16:44:55 +0530

    Update Assignment8.c

--- Assignment8.c
+++ Assignment8.c
@@ -2,7 +2,31 @@
 //input: Anitha
 //output: Aahint 
 
+#include <stdio.h>
+#include <string.h>
+
 int main()
 {
-  return 0;
+   char str[100];
+   int i, j, n;
+   char temp;
+
+   printf("Enter a string: ");
+   scanf("%s",&str[0]);
+   n=strlen(str);
+
+   for (i=0;i<n-1;i++)
+   {
+      for (j=i+1;j<n;j++)
+      {
+         if(str[i]>str[j])
+         {
+            temp=str[i];
+            str[i]=str[j];
+            str[j]=temp;
+         }
+      }
+   }
+   printf("\nSorted string is: %s", str);
+   return 0;
 }
