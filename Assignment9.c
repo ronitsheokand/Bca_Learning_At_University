70697b13dd2f3bbb0773c8f9ce863833ab73bda3
ronit7202 <128242677+ronitsheokand@users.noreply.github.com>
2023-04-27 16:45:34 +0530

    Update Assignment9.c

--- Assignment9.c
+++ Assignment9.c
@@ -5,8 +5,34 @@
 //.       m = 1
 //.       n = 1
 
-int main()
-{
-  
-  return 0;
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[100];
+    int freq[26] = {0};
+    int i, len;
+
+    printf("Enter your name: ");
+    scanf("%s",name);
+
+    len=strlen(name);
+
+    for(i=0;i<len;i++) {
+        if(name[i]>='a'&&name[i]<='z') {
+            freq[name[i]-'a']++;
+        }
+        else if(name[i]>='A'&&name[i]<='Z') {
+            freq[name[i]-'A']++;
+        }
+    }
+
+    printf("Frequency of each letter in the name:\n");
+    for(i = 0; i < 26; i++) {
+        if(freq[i] != 0) {
+            printf("%c = %d\n",i+'a',freq[i]);
+        }
+    }
+
+    return 0;
 }
